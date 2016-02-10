ff85b017e12f83b662042e75c7518e4be81fae79
Noviny BC <benjaminconolly@gmail.com>
2016-02-11 04:02:31 +1100

    WHATEVER

--- app/models/mentorship.rb
+++ app/models/mentorship.rb
@@ -2,8 +2,8 @@
 #
 # Table name: mentorships
 #
-#  user_a_id :integer          not null
-#  user_b_id :integer          not null
+#  user_id   :integer          not null
+#  mentor_id :integer          not null
 #
 
 class Mentorship < ActiveRecord::Base
