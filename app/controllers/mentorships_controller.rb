9442958452d32709d50506cd10021220bcbcbe8d
Noviny BC <benjaminconolly@gmail.com>
2016-02-12 09:26:59 +1100

    add mentors working

--- app/controllers/mentorships_controller.rb
+++ app/controllers/mentorships_controller.rb
@@ -9,23 +9,16 @@ class MentorshipsController < ApplicationController
   end
 
   def create
-    raise 'questions'
-    @mentorship.create(:user_id => @current_user.id, :mentor_id => 3)
-    @question = Question.find(@answer.question_id)
-    respond_to do |format|
-      if @answer.save
-        new_notification = Notification.create({
-          :user_id => @question.user_id,
-          :question_id => @question.id,
-          :answer_id => @answer.id
-        })
-
-        format.html { redirect_to @answer, notice: 'Answer was successfully created.' }
-        format.json { render :show, status: :created, location: @answer }
+    @mentorship = Mentorship.create(:user_id => @current_user.id, :mentor_id => params[:mentor_id])
+      if @mentorship.save
+        # new_notification = Notification.create({
+        #   :user_id => @question.user_id,
+        #   :question_id => @question.id,
+        #   :answer_id => @answer.id
+        # })
+        redirect_to mentors_path
       else
-        format.html { render :new }
-        format.json { render json: @answer.errors, status: :unprocessable_entity }
-      end
+        redirect_to root_path
     end
   end
 
